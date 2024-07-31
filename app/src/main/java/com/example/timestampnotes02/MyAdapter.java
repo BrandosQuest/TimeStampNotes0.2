f52ec425559acf930621785af02ab777228a00c9
Brando Calderara <151777266+BrandosQuest@users.noreply.github.com>
2024-07-31 21:16:38 +0200

    added files of first inizialisation of project

---
+++ app/src/main/java/com/example/timestampnotes02/MyAdapter.java
@@ -0,0 +1,43 @@
+package com.example.timestampnotes02;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<Item> itemList;
+
+    public MyAdapter(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        holder.textView.setText(item.getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+}
